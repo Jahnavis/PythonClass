ceb741a3ff0598c8fac31a2e11027172e55f5a3e
Jahnavi Murthy <Jahnavi Murthy>
2016-01-30 16:22:21 -0700

    Initial commit

---
+++ conditional.py
@@ -0,0 +1,8 @@
+x=7
+
+day= "Friday"
+if x<10:
+    print ("Today is" +day+ ".Its a great day."+str(x)+"Value of x")
+else:
+    print ("edg")
+    
---
+++ iteration.py
@@ -0,0 +1,8 @@
+books = ["Inheritance","The Oz","OOTD"]
+nums = range(100)
+
+for book in books:
+    print (book)
+    
+for n in nums:
+    print(n)
